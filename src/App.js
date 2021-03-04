07e332c04895c0bd9f7b2769afdc7cd329a2a8d5
Kato <kubkate@gmail.com>
2021-03-04 13:55:33 -0800

    set up pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Menu from './pages/Menu';
+import Gallery from './pages/Gallery';
+import Contact from './pages/Contact';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     this is App
-    </div>
+    <React.Fragment>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/menu' component={Menu} />
+        <Route exact path='/gallery' component={Gallery} />
+        <Route exact path='/contact' component={Contact} />
+      </Switch>
+    </React.Fragment>
   );
 }
 
